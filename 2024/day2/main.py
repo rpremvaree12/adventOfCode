210e8196f3d3197e96b9f20c23d4f17c3dcd0cac
rpremvaree12 <richard.premvaree@gmail.com>
2024-12-05 07:03:07 -0500

    mfa work session

--- 2024/day2/main.py
+++ 2024/day2/main.py
@@ -4,53 +4,37 @@ with open('sampleData.txt') as file:
 with open('inputData.txt') as file:
     rawInputData = file.read()
 
-
 def parseData(data):
     reportData = []
     for level in data.split('\n'):
         reportData.append([int(n) for n in level.split()])
     return reportData
 
-def checkReport(reportData):
+def isSafe(reportData):
     numSafe = 0
     for report in reportData:
-        isSafe = True
-        if report[0] > report[1]:
-            # levels should be decreasing
-            isSafe = checkDecreasingLevels(report)
-        else:
-            # levels should be increasing
-            isSafe = checkIncreasingLevels(report)
-        print(report)
-        if isSafe: numSafe += 1
-        # if isSafe:
-        #     print("Safe")
-        # else:
-        #     print("Unsafe")
-        # if isSafe: numSafe += 1
+        inc_or_dec = (report == sorted(report))or (report == sorted(report,reverse=True))
+        # if inc_or_dec and differenceOfThree(report):
+        #     numSafe += 1
     return numSafe
 
-
-def checkDecreasingLevels(report):
-    numError = 0
+def differenceOfThree(report):
     for i in range(len(report)-1):
-        if report[i] <= report[i+1] or (report[i+1] - report[i]) < -3:
-            numError += 1
-            if numError >2:
-                return False
-    return True
+        if abs(report[i] - report[i+1]) < 3 and report[i] - report[i+1] > 1:
+            print(report)
+            return True
+    return False
+# Part Two
+"""
+7 6 4 2 1: Safe without removing any level.
+1 2 7 8 9: Unsafe regardless of which level is removed.
+9 7 6 2 1: Unsafe regardless of which level is removed.
+1 3 2 4 5: Safe by removing the second level, 3.
+8 6 4 4 1: Safe by removing the third level, 4.
+1 3 6 7 9: Safe without removing any level.
+"""
 
-def checkIncreasingLevels(report):
-    numError = 0
-    for i in range(len(report)-1):
-        if report[i] >= report[i+1] or (report[i+1] - report[i]) > 3:
-            numError += 1
-            if numError >2:
-                return False
-    return True
-
-sampleData = parseData(rawSampleData)
-print(checkReport(sampleData))
+# Part One
 """
 7 6 4 2 1: Safe because the levels are all decreasing by 1 or 2.
 1 2 7 8 9: Unsafe because 2 7 is an increase of 5.
@@ -59,5 +43,8 @@ print(checkReport(sampleData))
 8 6 4 4 1: Unsafe because 4 4 is neither an increase or a decrease.
 1 3 6 7 9: Safe because the levels are all increasing by 1, 2, or 3.
 """
-inputData = parseData(rawInputData)
-print(checkReport(inputData))
+# inputData = parseData(rawInputData)
+# print(isSafe(inputData))
+
+sampleData = parseData(rawSampleData)
+print(isSafe(sampleData))
