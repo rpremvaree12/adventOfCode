70cd7f165c32e4e8d4ab33989633a0473ed8fe41
rpremvaree12 <richard.premvaree@gmail.com>
2024-12-11 21:57:25 -0500

    day3 partone complete

--- 2024/day3/day3.py
+++ 2024/day3/day3.py
@@ -1,12 +1,54 @@
 import re
 
-sampleText = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+with open('inputData.txt') as file:
+    inputData = "do()" + file.read()
+    inputData = inputData.split()
 
-cleanText = re.findall("/mul\(.*?\)/g",sampleText)
+sampleText1 = "do()" + "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+
+cleanSampleText1 = re.findall("mul\(\d{1,3},\d{1,3}\)",sampleText1)
+
+sampleText2 ="do()"+"xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+cleanSampleText2 = sampleText2.split('do()')
+
+
+def sumMuls(line):
+    sum = 0
+    cleanLine = re.findall("mul\(\d{1,3},\d{1,3}\)",line)
+    # print(cleanLine)
+    for t in cleanLine:
+        t = t[4:-1]
+        nums = [int(num) for num in t.split(',')]
+        sum += nums[0] * nums[1]
+    return sum
+
+def findDont(line):
+    sum = 0
+    match = "don't()"
+    cleanLine = line.split("do()")
+    for l in cleanLine:
+        l = re.sub(f"{match}.*", "", l)
+        sum += sumMuls(l)
+    return sum
+
+def partTwo(data):
+    totalSum = 0
+    for l in data:
+        totalSum += findDont(l)
+    return totalSum
+
 # The pattern \(.*?\):
 # \(: Matches an opening parenthesis.
 # .*?: Matches any number of characters (non-greedy).
 # \): Matches a closing parenthesis.
+# print(sumMuls(cleanSampleText))
 
-for t in cleanText:
-    print(t.split("mul("))
+def partOne(data):
+    totalSum = 0
+    for l in data:
+        totalSum+=sumMuls(l)
+        
+    return totalSum
+
+# print(partOne(inputData))
+print(partTwo(inputData))
